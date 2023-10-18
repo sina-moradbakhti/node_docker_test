d59b9d38d837c87a2cd2f3b5449f7ae48867f8f3
Sina Moradbakhti <sina.moradbakhti@gmail.com>
2023-10-18 14:23:41 +0330

    updated client message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, GeekMeek!, this is a docker test project');
 });
 
 app.listen(port, () => {
