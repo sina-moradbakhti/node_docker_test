917dce38062afc0226d85e04602c744737619708
Sina Moradbakhti <sina.moradbakhti@gmail.com>
2023-10-18 16:12:55 +0330

    updated the message for second time

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, GeekMeek!, this is a docker test project, updated from my local computer');
+  res.send('Hello, GeekMeek!, this is a docker test project, updated from my local computer, second part of my video');
 });
 
 app.listen(port, () => {
